1fd638b6a2cb165647efebc4d44716dc64dd3c38
BootsManuva <bootsmanuva@outlook.com>
2025-07-04 19:22:34 +0100

    define word count function and write ne main to complette code

--- main.py
+++ main.py
@@ -3,12 +3,16 @@ def get_book_text(book_path):
         book_contents = book.read()
     return book_contents
 
+def word_count(book_contents):
+    words = book_contents.split()
+    return len(words)
+
 def main():
     book_text = get_book_text("books/frankenstein.txt")
-    print(book_text)
+    count = word_count(book_text)
+    word_count_message = f"{count} words found in the document"
+    print(word_count_message)
 
 main()
 
-## get book text function defined. main function defined and then called
-## the main funchin does not need any arguments because it provides the input for the book file itself and then prints it 
-##
+
