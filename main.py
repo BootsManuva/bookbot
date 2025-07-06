a36d98c6a6c6abd20f1248ecd21a07a8753991aa
BootsManuva <bootsmanuva@outlook.com>
2025-07-06 13:43:17 +0100

    creasted mew fuunctions sor_on and list_char_dict, updated main to print out a word count and charater count report

--- main.py
+++ main.py
@@ -1,11 +1,24 @@
-from stats import get_book_text, word_count
+from stats import get_book_text,word_count, character_count, list_char_dict
 
 def main():
     book_text = get_book_text("books/frankenstein.txt")
     count = word_count(book_text)
-    word_count_message = f"{count} words found in the document"
+    word_count_message = f"Found {count} total words"
+    #print(word_count_message)
+
+    character_dict = character_count(book_text)
+    char_list = list_char_dict(character_dict)
+    
+    print("============ BOOKBOT ============")
+    print("Analyzing book found at books/frankenstein.txt...")
+    print("----------- Word Count ----------")
     print(word_count_message)
-
+    print("--------- Character Count -------")
+    for i in char_list:
+        c = i["char"]
+        n= i["num"]
+        if c.isalpha() == True:
+            print(f"{c}: {n}")
+    print("============= END ===============")
+    
 main()
-
-
--- stats.py
+++ stats.py
@@ -10,3 +10,29 @@ def get_book_text(book_path):
 def word_count(book_contents):
     words = book_contents.split()
     return len(words)
+
+def character_count(book_contents):
+    contents_lower_case = book_contents.lower()
+    character_count_dict ={}
+    for character in contents_lower_case:
+        if character in character_count_dict:
+            character_count_dict[character] = character_count_dict[character] + 1
+        else:
+            character_count_dict[character] = 1
+    return character_count_dict
+
+#ef list_dict_char(character_dict_count):
+#   char_list = []
+#   for item in character_dict_count:
+        
+def sort_on(items):
+    return items["num"]
+
+def list_char_dict(dict):
+    list_dict = []
+    for d in dict:
+        num = dict[d]
+        mini_dict = {"char": d, "num": num}
+        list_dict.append(mini_dict)
+    list_dict.sort(reverse=True, key=sort_on)
+    return list_dict
